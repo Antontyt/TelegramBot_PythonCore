50c2db3fe59d6b902726e600c1535464dbbbd398
Antontyt <antontyt@gmail.com>
2026-07-07 21:28:15 +0300

    feat: add basic bot functionality

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import asyncio
 from os import getenv
 
 from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 
 load_dotenv()
 
@@ -10,5 +14,18 @@ if not TELEGRAM_TOKEN or not TELEGRAM_TOKEN.strip():
         "TELEGRAM_TOKEN не найден или пустой. "
         "Проверь, что в .env есть строка TELEGRAM_TOKEN=твой_токен"
     )
+print("Успешно получен TELEGRAM_TOKEN")
 
-print(f"Полученный токен для телеграмм: {TELEGRAM_TOKEN}")
+bot = Bot(token=TELEGRAM_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer("Hello! I'm a bot and this is my first message.")
+
+async def main():
+    print("Бот успешно запущен")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
